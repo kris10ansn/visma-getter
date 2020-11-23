aeaa3a68b85470bf0f8a88139f384a06c15872ea
kris10ansn <telefon.kristian@gmail.com>
2020-11-23 17:23:03 +0100

    refactor to use dayjs

---
+++ client/src/util/useLiveDate.ts
@@ -0,0 +1,45 @@
+import dayjs from "dayjs";
+import { useEffect, useMemo, useState } from "react";
+import { FixedArray } from "./FixedArray";
+
+export enum Unit {
+    Year = 0,
+    Month = 1,
+    Day = 2,
+    Hour = 3,
+    Minute = 4,
+    Second = 5,
+    MilliSecond = 6,
+}
+
+export const useLiveDate = (unit: Unit) => {
+    const initial = useMemo(() => dayjs(), []);
+    const [date, setDate] = useState<dayjs.Dayjs>(initial);
+
+    useEffect(() => {
+        const values = [
+            date?.year(),
+            date?.month(),
+            date?.date(),
+            date?.hour(),
+            date?.minute(),
+            date?.second(),
+            date?.millisecond(),
+        ].map((value, index) => {
+            if (index === unit) {
+                return value! + 1;
+            } else if (index > unit) {
+                return 0;
+            }
+
+            return value;
+        }) as FixedArray<number, 7>;
+
+        const next = dayjs(new Date(...values));
+        const delay = next.valueOf() - date.valueOf();
+        const timeout = setTimeout(setDate, delay, next);
+        return () => clearTimeout(timeout);
+    }, [unit, date, setDate]);
+
+    return date;
+};
