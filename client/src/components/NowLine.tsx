b74d3ff158f791498e0686251a140d642c216894
kris10ansn <telefon.kristian@gmail.com>
2020-11-23 17:23:48 +0100

    refactor, add time constraint

---
+++ client/src/components/NowLine.tsx
@@ -0,0 +1,32 @@
+import dayjs from "dayjs";
+import React from "react";
+import { Unit, useLiveDate } from "src/util/useLiveDate";
+
+const num = (date: dayjs.Dayjs) => {
+    return (
+        date.hour() +
+        date.minute() / Math.pow(60, 1) +
+        date.second() / Math.pow(60, 2)
+    );
+};
+
+const NowLine: React.FC = () => {
+    const now = useLiveDate(Unit.Second);
+    const time = num(now);
+
+    return time >= 8 && time <= 15 ? (
+        <div
+            style={{
+                width: "100%",
+                height: "3px",
+                background: "red",
+                position: "absolute",
+                top: `${((time - 8) / 8) * 1000}px`,
+            }}
+        ></div>
+    ) : (
+        <></>
+    );
+};
+
+export default NowLine;
