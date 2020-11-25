78d2f9525fa4a6b049a00302d81a6bc0d6a86f43
kris10ansn <telefon.kristian@gmail.com>
2020-11-25 20:11:30 +0100

    shrink timetableItems by 0.01 on top and bottom to prevent intersection between consecutive items

--- client/src/util/intersects.ts
+++ client/src/util/intersects.ts
@@ -3,8 +3,8 @@ import num from "src/util/numFromTime";
 import { ITimeTableItem } from "src/util/TimeTableInfo";
 
 export const intersects = (a: ITimeTableItem, b: ITimeTableItem) => {
-    const rangeA = [num(a.startTime), num(a.endTime)];
-    const rangeB = [num(b.startTime), num(b.endTime)];
+    const rangeA = [num(a.startTime) + 0.01, num(a.endTime) - 0.01];
+    const rangeB = [num(b.startTime) + 0.01, num(b.endTime) - 0.01];
 
     const min = math.min(rangeA) < math.min(rangeB) ? rangeA : rangeB;
     const max = math.max(rangeA) > math.max(rangeB) ? rangeA : rangeB;
