6f484cd2e42800e89e05b8688d257e9212db9c80
kris10ansn <telefon.kristian@gmail.com>
2020-11-30 09:11:35 +0100

    update NowLine.tsx

--- client/src/components/NowLine.tsx
+++ client/src/components/NowLine.tsx
@@ -1,14 +1,25 @@
 import dayjs from "dayjs";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Unit, useLiveDate } from "src/util/useLiveDate";
 
 const num = (date: dayjs.Dayjs) => {
-    return date.hour() + date.minute() / Math.pow(60, 1);
+    return (
+        date.hour() +
+        date.minute() / Math.pow(60, 1) +
+        date.second() / Math.pow(60, 2)
+    );
 };
 
+const constrain = (n: number) => Math.max(8, Math.min(15.5, n));
+
 const NowLine: React.FC = () => {
-    const now = useLiveDate(Unit.Minute);
-    const time = Math.max(8, Math.min(15.25, num(now)));
+    const now = useLiveDate(Unit.Second);
+    const [time, setTime] = useState<number>(constrain(num(now)));
+
+    useEffect(() => {
+        setTime(constrain(num(now)));
+        console.log(now);
+    }, [now]);
 
     return (
         <div
