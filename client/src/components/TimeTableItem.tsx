70883e750745e59d4ab9c27c9c8f3cbcb5cfd2c6
kris10ansn <telefon.kristian@gmail.com>
2020-11-30 23:51:17 +0100

    add react-icons

--- client/src/components/TimeTableItem.tsx
+++ client/src/components/TimeTableItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ITimeTableItem } from "src/util/TimeTableInfo";
 import "src/components/TimeTableItem.scss";
+import { HiOutlineLocationMarker } from "@react-icons/all-files/hi/HiOutlineLocationMarker";
+import { HiOutlineClock } from "@react-icons/all-files/hi/HiOutlineClock";
 
 interface Props {
     top: string;
@@ -24,18 +26,33 @@ const norwegian = (type: string) => {
 };
 
 const TimeTableItem: React.FC<Props> = ({ item, top, height }) => {
-    const styles: React.CSSProperties = {
-        top,
-        height,
-        borderColor: `#ccc #ccc #ccc #${item.colour}`,
+    const styles: { [key: string]: React.CSSProperties } = {
+        main: {
+            top,
+            height,
+            borderColor: `#ccc #ccc #ccc #${item.colour}`,
+        },
+        icon: {
+            transform: "translateY(14.5%)",
+            marginRight: 4,
+            marginLeft: 4,
+        },
+        info: {
+            marginLeft: -4,
+        },
     };
+
     return (
-        <div className="TimeTableItem" style={styles}>
-            <div className="info">
+        <div className="TimeTableItem" style={styles.main}>
+            <div className="info" style={styles.info}>
                 <small>
+                    <HiOutlineClock style={styles.icon} />
                     {item.startTime}-{item.endTime}
-                </small>{" "}
-                <small>{item.locations.join(" ")}</small>
+                </small>
+                <small>
+                    <HiOutlineLocationMarker style={styles.icon} />
+                    {item.locations.join(" ")}
+                </small>
             </div>
             <p>{item.subject || item.label}</p>
             <small>{item.subject ? item.label : ""}</small>
