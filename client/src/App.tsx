f9559093fa9681a1ef7d4ed2d2cdee47275e8a65
kris10ansn <telefon.kristian@gmail.com>
2020-12-02 19:14:36 +0100

    remove demonstrative inputs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,6 @@ import "./App.scss";
 const App: React.FC = () => {
     return (
         <div className="App">
-            <div className="interact">
-                <input type="text" />
-                <br />
-                <input type="text" />
-                <br />
-                <input type="text" />
-                <br />
-            </div>
             <div className="timeatable-container">
                 <TimeTable />
             </div>
