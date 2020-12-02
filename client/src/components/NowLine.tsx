c11ddab30f2c29803d13b888896f6bf1781c238a
kris10ansn <telefon.kristian@gmail.com>
2020-12-02 21:32:37 +0100

    limit time/clock position to be within bounds

--- client/src/components/NowLine.tsx
+++ client/src/components/NowLine.tsx
@@ -1,6 +1,5 @@
 import dayjs from "dayjs";
 import React, { RefObject, useRef } from "react";
-import inViewport from "src/util/inViewport";
 import { pos, style } from "src/util/pos";
 import { Unit, useLiveDate } from "src/util/useLiveDate";
 import "./NowLine.scss";
@@ -16,32 +15,61 @@ const num = (date: dayjs.Dayjs) => {
 const constrain = (n: number, min: number, max: number) =>
     Math.max(min, Math.min(max, n));
 
+(window as any)._offset = -21 + 8 - 0.5;
+
 const NowLine: React.FC = () => {
-    const timeElement = useRef() as RefObject<HTMLDivElement>;
-    const getTime = (now: dayjs.Dayjs) => constrain(num(now), 8, 15.475);
+    const getTime = (now: dayjs.Dayjs) =>
+        constrain(num(now) + (window as any)._offset, 8, 15.475);
     const now = useLiveDate(Unit.Second);
     const position = pos(getTime(now));
+    const timeSpanCopy = useRef() as RefObject<HTMLDivElement>;
+    const timeContainer = useRef() as RefObject<HTMLDivElement>;
 
-    const styles = {
+    const styles: { [key: string]: React.CSSProperties } = {
         container: {
             top: style(position),
+            bottom: "",
         },
         span: {
             transform: "translateY(-40%)",
         },
+        spanCopy: {
+            transform: "translateY(-40%)",
+            visibility: "hidden",
+        },
     };
 
-    if (timeElement.current && !inViewport(timeElement.current)) {
-        styles.span["transform"] = "translate(0)";
-        const prop = getTime(now) > 15 ? "bottom" : "top";
-        styles.span[prop] = -1;
+    if (timeSpanCopy.current && timeContainer.current) {
+        const timetable = document
+            .querySelector("div.TimeTable")!
+            .getBoundingClientRect();
+
+        const containerBounds = timeContainer.current.getBoundingClientRect();
+        const bounds = timeSpanCopy.current.getBoundingClientRect();
+
+        if (bounds.top < timetable.top) {
+            styles.span.transform = "translate(0)";
+            styles.span.top = timetable.top - containerBounds.top;
+        }
+
+        if (bounds.bottom > timetable.bottom) {
+            styles.span.transform = "translate(0)";
+            styles.span.bottom = containerBounds.bottom - timetable.bottom;
+        }
     }
 
     return (
         <div className="NowLine" style={styles.container}>
-            <div className="time-container">
-                <span className="time" style={styles.span} ref={timeElement}>
-                    {now.format("HH:mm:ss")}
+            <div className="time-container" ref={timeContainer}>
+                <span
+                    className="time"
+                    style={styles.spanCopy}
+                    ref={timeSpanCopy}
+                >
+                    00:00:00
+                </span>
+                <span className="time" style={styles.span}>
+                    {now.format("15:mm:ss")}
                 </span>
             </div>
         </div>
