31bc4523a832dcf523049f70e1fbc31384221b68
kris10ansn <telefon.kristian@gmail.com>
2020-12-02 20:03:35 +0100

    add inViewport util function

---
+++ client/src/util/inViewport.ts
@@ -0,0 +1,13 @@
+const inViewport = (element: HTMLElement) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+export default inViewport;
